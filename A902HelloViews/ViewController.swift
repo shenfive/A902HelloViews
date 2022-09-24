69c0b54ba37d7596b0d409abbe049c23957bceb6
Danny Shen <shenfive@gmail.com>
2022-09-24 13:12:44 +0800

    轉動

--- A902HelloViews/ViewController.swift
+++ A902HelloViews/ViewController.swift
@@ -9,9 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var theView:UIView = UIView()
+    
+    var counter = 0.0
+    var timer:Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        theView.bounds = CGRect(x: 0, y: 0, width: 200, height: 200)
+        theView.center = self.view.center
+        theView.backgroundColor = UIColor.red
+        
+        counter = 45
+        setRotate()
+        
+        
+        theView.layer.cornerRadius = 15
+        theView.clipsToBounds = true
+        
+      
+        
+        self.view.addSubview(theView)
+        
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0/60, target: self, selector: #selector(setRotate), userInfo: nil, repeats: true)
+        timer.fire()
+        
+        
+    }
+    
+    @objc func setRotate(){
+        let angle =  counter * Double.pi / 180
+        theView.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
+        counter += 1
     }
 
 
